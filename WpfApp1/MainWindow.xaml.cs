ce603bfb0a4ff37abc20b607dabe82bc1144dfe2
Ali <136814140+aliaksu0@users.noreply.github.com>
2024-12-13 17:44:43 +0300

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Data;
+using System;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CMPE312_PROJECT;Integrated Security=True;Encrypt=False";
+            sqlConnection = new SqlConnection(connectionString);
+            ShowBusList();
+            ShowDriverList();
+            ShowPassengerList();
+        }
+        private void ShowBusList()
+        {
+            try
+            {
+                string query = "select * from Bus";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable busTable = new DataTable();
+                    sqlDataAdapter.Fill(busTable);
+                    buslist.DisplayMemberPath = "plate";
+                    buslist.SelectedValuePath = "Id";
+                    buslist.ItemsSource = busTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        private void ShowDriverList()
+        {
+            try
+            {
+                string query = "select * from Driver";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable driverTable = new DataTable();
+                    sqlDataAdapter.Fill(driverTable);
+                    driverlist.DisplayMemberPath = "name";
+                    driverlist.SelectedValuePath = "Id";
+                    driverlist.ItemsSource = driverTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        private void ShowPassengerList()
+        {
+            try
+            {
+                string query = "select * from Passanger";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable passengerTable = new DataTable();
+                    sqlDataAdapter.Fill(passengerTable);
+                    passengerlist.DisplayMemberPath = "name";
+                    passengerlist.SelectedValuePath = "Id";
+                    passengerlist.ItemsSource = passengerTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void assignButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        SqlConnection sqlConnection;
+        public Window1()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CMPE312_PROJECT;Integrated Security=True;Encrypt=False";
+            sqlConnection = new SqlConnection(connectionString);
+            LoadComboBox1();
+            LoadComboBox2();
+        }
+        private void LoadComboBox1()
+        {
+            try
+            {
+                string query = "SELECT Id, name FROM Passanger";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable passengerTable = new DataTable();
+                    sqlDataAdapter.Fill(passengerTable);
+
+                    comboBox1.DisplayMemberPath = "name";
+                    comboBox1.SelectedValuePath = "Id";  
+                    comboBox1.ItemsSource = passengerTable.DefaultView; 
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Bir hata oluştu: {e.Message}");
+            }
+        }
+        private void LoadComboBox2()
+        {
+            try
+            {
+                string query = "SELECT Id, plate FROM Bus";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable busTable= new DataTable();
+                    sqlDataAdapter.Fill(busTable);
+
+                    comboBox2.DisplayMemberPath = "plate";
+                    comboBox2.SelectedValuePath = "Id";
+                    comboBox2.ItemsSource = busTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Bir hata oluştu: {e.Message}");
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
